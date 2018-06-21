bf6aac84696c00bfc6db2dd1ff0a046ec7734141
piet0024 <n.pietrantonio0024@live.com>
2018-06-21 13:37:34 -0400

    add test create form

--- src/containers/CreateMeeting.js
+++ src/containers/CreateMeeting.js
@@ -1,10 +1,22 @@
 import React from 'react';
 
+import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
+
 export default class CreateMeeting extends React.Component {
   render() {
     return (
       <div>
-                Create Meeting form
+        <Form>
+          <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
+            <Label for="exampleTitle" className="mr-sm-2">Title</Label>
+            <Input type="text" name="title" id="exampleTitle" placeholder="Weekly Team Meeting" />
+          </FormGroup>
+          <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
+            <Label for="exampleDescription" className="mr-sm-2">Description</Label>
+            <Input type="text" name="description" id="exampleDescription" placeholder="Descript" />
+          </FormGroup>
+          <Button>Submit</Button>
+        </Form>
       </div>
     );
   }
